f47b7f4bcae174c408f3436d629e510a28cdfba2
Lapidis2 <hitlapidis2@gmail.com>
2025-09-10 17:15:40 +0200

    caluculating financial annual savings

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,5 @@
+income = int(input("Enter your monthly income: "))
+expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = income - expenses
+Projected_savings = monthly_savings * 12 + monthly_savings * 12 * 0.05
+print("Your annual savings are:", Projected_savings)
