e6122d6240b32c04428b38418c0dcc8df53b2158
Lapidis2 <hitlapidis2@gmail.com>
2025-09-17 23:05:00 +0200

    solving errors in typing

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,7 +1,7 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-operator = input("Choose the operation (+, -, *, /): ")
-match operator:
+operation = input("Choose the operation (+, -, *, /): ")
+match operation:
     case "+":
         result = num1 + num2
         print(f"The result is {result}.")
