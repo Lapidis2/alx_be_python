eae7c860c278e5588914235d30d0f773a62ac5c1
Lapidis2 <hitlapidis2@gmail.com>
2025-10-05 18:47:07 +0200

    solving naming error

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -20,12 +20,12 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 5), -5)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         """Test multiplication functionality."""
         self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(-2, 5), -10)
 
-    def test_divide(self):
+    def test_division(self):
         """Test division functionality."""
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(9, 3), 3)
