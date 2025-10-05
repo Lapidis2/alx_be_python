1dfcf2fc002d732bdbe7ed4e8af0a464865fa8c4
Lapidis2 <hitlapidis2@gmail.com>
2025-10-05 18:39:28 +0200

    test simple calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,18 +1,29 @@
 
 
-class SimpleCalculator:
-    """A simple calculator class that supports basic arithmetic operations."""
+import unittest
+from simple_calculator import SimpleCalculator
 
-    def add(self, a, b):
-        return a + b
+class TestSimpleCalculator(unittest.TestCase):
 
-    def subtract(self, a, b):
-        return a - b
+    def setUp(self):
+        self.calc = SimpleCalculator()
 
-    def multiply(self, a, b):
-        return a * b
+    def test_add(self):
+        self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(-1, -1), -2)
 
-    def divide(self, a, b):
-        if b == 0:
-            return None
-        return a / b
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-2, 5), -10)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(9, 3), 3)
+        self.assertIsNone(self.calc.divide(5, 0))
+
+if __name__ == '__main__':
+    unittest.main()
